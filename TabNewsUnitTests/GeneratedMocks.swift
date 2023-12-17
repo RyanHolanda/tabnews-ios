b76941e732a0d765d7ee3b51047497bffef86d3c
Ryan Holanda <ryanholanda08@gmail.com>
2023-12-17 17:43:13 -0300

    wip

--- TabNewsUnitTests/GeneratedMocks.swift
+++ TabNewsUnitTests/GeneratedMocks.swift
@@ -1,4 +1,4 @@
-// MARK: - Mocks generated from file: TabNews/Core/HTTP.swift at 2023-12-17 8:04:54 PM +0000
+// MARK: - Mocks generated from file: TabNews/Core/HTTP.swift at 2023-12-17 8:25:22 PM +0000
 
 
 import Cuckoo
@@ -246,7 +246,7 @@ import Foundation
 
 
 
-// MARK: - Mocks generated from file: TabNews/Repositories/ContentRepository.swift at 2023-12-17 8:04:54 PM +0000
+// MARK: - Mocks generated from file: TabNews/Repositories/ContentRepository.swift at 2023-12-17 8:25:22 PM +0000
 
 
 import Cuckoo
